c0cbd91ca422bf9c8fc51bc53cb1cfd3cf8d15f3
NeerajQA <neeraj1264qa@gmail.com>
2024-12-20 13:50:46 +0530

    add size option in history product

--- models/order.js
+++ models/order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   id: { type: String, required: true },
-  products: [{ name: String, price: Number, quantity: Number }],
+  products: [{ name: String, price: Number, quantity: Number , size: String}],
   totalAmount: { type: Number, required: true },
   timestamp: { type: String, required: true },
 });
