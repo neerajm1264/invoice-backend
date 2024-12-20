d803a7b9ce8f11af533513aff3573689b1cfae9c
NeerajQA <neeraj1264qa@gmail.com>
2024-12-20 12:41:57 +0530

    added 5000 port

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.delete('/api/products', async (req, res) => {
 
 
 // Start server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
