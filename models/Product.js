9487a2ebb74e659dd0a22f0e6bc18dd615964a36
NeerajQA <neeraj1264qa@gmail.com>
2024-12-25 18:54:23 +0530

    ..

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  id: { type: String },
+  id: { type: mongoose.Schema.Types.Mixed, required: true },
   name: { type: String, required: true },
   price: { type: Number},
   image: { type: String, required: false },
   category: { type: String, required: true },
   varieties: [
     {
-      id: { type: String },
+      id: { type: mongoose.Schema.Types.Mixed, required: true },
       size: String,
       price: Number,
     }
