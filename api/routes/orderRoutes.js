a719f8f77e06dbff1318c104cf69fcc2deeaa16b
Neeraj <151511195+neerajm1264@users.noreply.github.com>
2025-06-19 21:45:03 +0530

    Update orderRoutes.js

--- api/routes/orderRoutes.js
+++ api/routes/orderRoutes.js
@@ -5,8 +5,8 @@ const Order = require('../models/order');
 // Create a new order
 router.post('/', async (req, res) => {
   try {
-    const { id, products, totalAmount, timestamp, name, phone, address } = req.body;
-    const newOrder = new Order({ id, products, totalAmount, timestamp, name, phone, address  });
+    const { id, products, totalAmount, timestamp, name, phone, address, discount, delivery } = req.body;
+    const newOrder = new Order({ id, products, totalAmount, timestamp, name, phone, address, discount, delivery  });
 
     await newOrder.save();
     res.status(201).json(newOrder);
